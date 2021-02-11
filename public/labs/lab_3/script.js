867d07a2f5eb212f1b1cbcfc369a31b17614b413
ZhengXiShen <78034421+ZhengXiShen@users.noreply.github.com>
2021-02-11 17:41:26 -0500

    no for

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,11 +1,6 @@
 /* Put your javascript in here */
 
-let i = 1;
-    for(let li of carousel.querySelectorAll('li')) {
-      li.style.position = 'relative';
-      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-      i++;
-    }
+
 
     /* configuration */
     let width = 130; // image width
