533f42818f07f0e3eab79f0b8e95de8238016a50
ZhengXiShen <78034421+ZhengXiShen@users.noreply.github.com>
2021-02-12 19:31:29 -0500

    Revert "final without array"
    This reverts commit 5f71fb77b3ab8a605d2a80293a1ed7b404dd5734.

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,28 +1,28 @@
 /* Put your javascript in here */
 
 
-function arrayImageList(){
-    const pictureArray = document.querySelectorAll('.mochi');
-    const arrayImage = document.querySelector('ul');
-    const allImages = document.querySelectorAll('li');
-    let width = 130; 
-    let count = 3; 
-    let position = 0;
-    
-    carousel.querySelector('.left').onclick = function goLeft() {
-                position += width * count;
-          position = Math.min(position, 0)
-          arrayImage.style.marginLeft = position + 'px';
-        };
-    
-        carousel.querySelector('.right').onclick = function goRight() {
-          position -= width * count;
-          position = Math.max(position, -width * (allImages.length - count));
-          arrayImage.style.marginLeft = position + 'px';
-        };
-    
-    
-    }
-    
-    
-    window.onload = arrayImageList;
+
+    /* configuration */
+    let width = 130; // image width
+    let count = 3; // visible images count
+
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // ribbon scroll position
+
+    carousel.querySelector('.left').onclick = function() {
+      // shift left
+      position += width * count;
+      // can't move to the left too much, end of images
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.right').onclick = function() {
+      // shift right
+      position -= width * count;
+      // can only shift the ribbbon for (total ribbon length - visible count) images
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
