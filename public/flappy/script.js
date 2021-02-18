d833472e656735ce513a9c67a0108187c544619f
ZhengXiShen <78034421+ZhengXiShen@users.noreply.github.com>
2021-02-18 02:23:29 -0500

    die stop

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -49,10 +49,13 @@ document.addEventListener('DOMContentLoaded', () =>{
                 gameDisplay.removeChild(obstacle)
             }
             if (
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
+                birdBottom < obstacleBottom + 153 ||
                 birdBottom === 0
                 ) {
                 gameOver()
+                clearInterval(timerId)
+
             }
         }
         let timerId = setInterval(moveObstacle, 20)
